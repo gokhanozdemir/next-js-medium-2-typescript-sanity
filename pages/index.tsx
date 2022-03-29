13f164e79eb5aff9968721c900629c7d8fb7366b
Gökhan <gn.ozdemir@gmail.com>
2022-03-29 13:00:19 +0300

    Add tailwind-lineclamp

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,13 +37,13 @@ export default function Home({ posts }: Props) {
         {posts.map((post) => {
           return (
             <Link key={post._id} href={`/post/${post.slug.current}`}>
-              <div className=''>
-                <img src={urlFor(post.mainImage).url()} alt={post.title} />
+              <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+                <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()} alt={post.title} />
                 <div className='flex justify-between p-5 bg-white'>
                   <div>
-                    <p>{post.title}</p>
-                    <p>
-                      {post.description} by {post.author.name}
+                    <p className='text-lg font-bold'>{post.title}</p>
+                    <p className='text-sm '>
+                      <span className='line-clamp-1'>{post.description}</span> by {post.author.name}
                     </p>
                   </div>
                   <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()} alt={post.author.name} />
