c6530c63f164e0d7093a2da30174771f77245db3
Gökhan <gn.ozdemir@gmail.com>
2022-03-30 13:18:16 +0300

    Render Body
    Add react-portable-text and create serializer strings for interface

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,6 +2,8 @@ import { GetStaticProps } from "next"
 import Header from "../../components/Header"
 import { sanityClient, urlFor } from "../../sanity"
 import { PostTypeInterface } from "../../typings"
+import PortableText from "react-portable-text"
+
 
 interface Props {
 	post: PostTypeInterface
@@ -25,9 +27,32 @@ function Post({ post }: Props) {
 					<p className="font-extralight text-sm">Blog post by <span className="text-green-600">{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString('tr-TR')}</p>
 				</div>
 
+				<div className="">
+					<PortableText
+						className=""
+						dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+						projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+						content={post.body}
+						serializers={{
+							h1: (props: any) => {
+								return <h1 className="text-2xl font-bold my-5" {...props} />
+							},
+							h2: (props: any) => {
+								return <h2 className="text-xl font-bold my-5" {...props} />
+							},
+							li: ({ children }: any) => {
+								<li className="text-xl font-bold my-5" >{children}</li>
+							},
+							link: ({ href, children }: any) => {
+								<a href={href} className="text-blue-500 hover:underline" >{children}</a>
+							},
+						}}
+					/>
+				</div>
 			</article>
 
 		</main >
+
 
 	)
 }
